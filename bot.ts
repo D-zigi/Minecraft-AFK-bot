f05a3a7746e9987cd2d1fa55b824ccb32d27c38d
Alexey <yusufovalexey@gmail.com>
2026-04-19 00:39:11 +0300

    feat: initialize afk bot project with essential configurations
    - Added package.json for project dependencies and scripts
    - Created settings.json for bot configuration including server details and utilities
    - Introduced tsconfig.json for TypeScript compiler options and settings

---
+++ bot.ts
@@ -0,0 +1,95 @@
+const mineflayer = require("mineflayer") as typeof import("mineflayer");
+const { pathfinder, Movements, goals } = require("mineflayer-pathfinder") as typeof import("mineflayer-pathfinder");
+const config = require("./settings.json") as typeof import("./settings.json");
+
+const { GoalBlock } = goals;
+
+function createBot() {
+  const bot = mineflayer.createBot({
+    username: config["bot-account"]["username"],
+    password: config["bot-account"]["password"],
+    auth: config["bot-account"]["type"] as "mojang" | "microsoft" | "offline",
+    host: config.server.ip,
+    port: config.server.port,
+    version: config.server.version,
+  });
+
+  bot.loadPlugin(pathfinder);
+  const defaultMove = new Movements(bot);
+
+  bot.once("spawn", () => {
+    console.log("\x1b[33m[BotLog] Bot joined to the server", "\x1b[0m");
+
+    if (config.utils["auto-auth"].enabled) {
+      console.log("[INFO] Started auto-auth module");
+
+      const password = config.utils["auto-auth"].password;
+      setTimeout(() => {
+        bot.chat(`/register ${password} ${password}`);
+        bot.chat(`/login ${password}`);
+      }, 500);
+
+      console.log(`[Auth] Authentification commands executed.`);
+    }
+
+    if (config.utils["chat-messages"].enabled) {
+      console.log("[INFO] Started chat-messages module");
+      const messages = config.utils["chat-messages"]["messages"];
+
+      if (config.utils["chat-messages"].repeat) {
+        const delay = config.utils["chat-messages"]["repeat-delay"];
+        let i = 0;
+
+        setInterval(() => {
+          bot.chat(`${messages[i]}`);
+
+          if (i + 1 === messages.length) {
+            i = 0;
+          } else i++;
+        }, delay * 1000);
+      } else {
+        messages.forEach((msg) => {
+          bot.chat(msg);
+        });
+      }
+    }
+
+    const pos = config.position;
+
+    if (config.position.enabled) {
+      console.log(
+        `\x1b[32m[BotLog] Starting moving to target location (${pos.x}, ${pos.y}, ${pos.z})\x1b[0m`,
+      );
+      bot.pathfinder.setMovements(defaultMove);
+      bot.pathfinder.setGoal(new GoalBlock(pos.x, pos.y, pos.z));
+    }
+
+    if (config.utils["anti-afk"].enabled) {
+      bot.setControlState("jump", true);
+      if (config.utils["anti-afk"].sneak) {
+        bot.setControlState("sneak", true);
+      }
+    }
+  });
+
+  if (config.utils["auto-reconnect"]) {
+    bot.on("end", () => {
+      setTimeout(() => {
+        createBot();
+      }, config.utils["auto-reconnect-delay"]);
+    });
+  }
+
+  bot.on("kicked", (reason) =>
+    console.log(
+      "\x1b[33m",
+      `[BotLog] Bot was kicked from the server. Reason: \n${reason}`,
+      "\x1b[0m",
+    ),
+  );
+  bot.on("error", (err) =>
+    console.log(`\x1b[31m[ERROR] ${err.message}`, "\x1b[0m"),
+  );
+}
+
+createBot();
